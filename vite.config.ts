acb173a347b0ab08e57a0a8ae68c2475c8e89207
Locnguyen <134362330+LoricLocnguyen@users.noreply.github.com>
2026-05-02 17:53:18 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // Tên repo của bạn phải khớp hoàn toàn ở đây
-  base: '/storegithub/', 
+  // Sửa từ '/storegithub/' thành '/' để chạy trên Vercel
+  base: '/', 
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  // Ép Vite tối ưu hóa thư viện để tránh lỗi "QueryClientProvider is not defined"
+  optimizeDeps: {
+    include: ['@tanstack/react-query'],
   },
 });
