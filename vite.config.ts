962ced4c54c5cecd7fb8ff7d67a098022f27240d
Locnguyen <134362330+LoricLocnguyen@users.noreply.github.com>
2026-05-02 16:56:07 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Quan trọng: base phải khớp với tên repository của bạn trên GitHub
+  // Tên repo của bạn phải khớp hoàn toàn ở đây
   base: '/storegithub/', 
-  
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  
-  plugins: [
-    react( ),
-    // Đã xóa componentTagger() của Lovable ở đây
-  ],
-  
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
